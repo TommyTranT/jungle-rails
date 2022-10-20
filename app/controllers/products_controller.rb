89a27e687a19bac76f6b96e2b754003586e581a8
Tommy Tran <haitran1995@gmail.com>
2022-10-20 08:10:27 -0400

    first commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,10 @@
 class ProductsController < ApplicationController
 
   def index
+    # uses MODEL
     @products = Product.all.order(created_at: :desc)
+    # render VIEW. Bottom line is optional because is is implied
+    render :index
   end
 
   def show
